e51709d004cf3456765fe5d9988ddfac3f073637
uday <udaychowdary303@gmail.com>
2022-10-24 21:06:52 +0530

    updates

--- src/app/modules/dashboard/reactivate-success/reactivate-success.component.ts
+++ src/app/modules/dashboard/reactivate-success/reactivate-success.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reactivate-success',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactivateSuccessComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
 
+  onContinue(){
+    this.router.navigate([`dashboard`]);
+ }
 }
--- src/app/modules/dashboard/unlock-user-account/unlock-user-account.component.ts
+++ src/app/modules/dashboard/unlock-user-account/unlock-user-account.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-unlock-user-account',
@@ -7,9 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UnlockUserAccountComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
+
+  onContinue(){
+     this.router.navigate([`dashboard`]);
+
+  }
+
 
 }
